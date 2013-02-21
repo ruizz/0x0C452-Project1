79cae1246584161c1aa431259797be1eadf811eb
drewsatmid <drewsatmid@tamu.edu>
2013-02-20 19:01:05 -0600

    Update Project1/main.cpp

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -5,20 +5,36 @@
 #include <FL/Fl_Widget.H>
 #include <FL/Fl_Button.H>
 #include <../rotation.h>
+#include <vector>
 
 #define MIDDLEX 475
+
+
+struct Pair{
+	int x;
+	int y;
+	Pair(int x1, int y1) {
+		x = x1;
+		y = y1;
+	}
+};
 
 int angle1 = 0;
 int angle2 = 0;
 int angle3 = 0;
 bool painting = false;
+std::vector<Pair> pairs;
 
 Lines* arms;
+
 
 void Drawing::draw()
 {
 	fl_color(FL_WHITE);
 	//draw_line(XX+0,0,0,150);
+	for (int i=0; i<pairs.size();i++){
+		fl_pie(pairs[i].x, pairs[i].y, 10, 10, 0, 360);
+	}
 	draw_line(arms->lines[0]->x1, arms->lines[0]->y1, arms->lines[0]->x2, arms->lines[0]->y2);
 	fl_color(FL_WHITE);
 	//draw_line(XX+0,150,0,100);
@@ -26,9 +42,30 @@ void Drawing::draw()
 	fl_color(FL_WHITE);
 	//draw_line(XX+0,250,0,75);
 	draw_line(arms->lines[2]->x1, arms->lines[2]->y1, arms->lines[2]->x2, arms->lines[2]->y2);
+
+
+	fl_color(255, 255, 255);
+	//int x1 = (int)arms->lines[0]->x1, BY-arms->lines[0]->y1;
+	//int y1 = y()-5;
+	fl_pie(BX+arms->lines[0]->x1-5, BY-arms->lines[0]->y1-5, 10, 10, 0, 360);
+
+	//int x1 = x()-5;
+	//int y1 = y()-5;
+	fl_pie(BX+arms->lines[1]->x1-5, BY-arms->lines[1]->y1-5, 10, 10, 0, 360);
+
+	//int x1 = x()-5;
+	//int y1 = y()-5;
+	fl_pie(BX+arms->lines[2]->x1-5, BY-arms->lines[2]->y1-5, 10, 10, 0, 360);
 	//fl_arc(point(0).x,point(0).y,r+r,r+r,0,360);
 	//fl_arc(100, 100, 200, 200, 0, 360);
 	//fl_pie(0,0,200,200,0,360);
+
+	fl_pie(BX+arms->lines[2]->x2-5, BY-arms->lines[2]->y2-5, 10, 10, 0, 360);
+
+	if(painting){
+		pairs.push_back(Pair(BX+arms->lines[2]->x2-5, BY-arms->lines[2]->y2-5));
+	}
+
 	fl_end_line();
 }
 
@@ -65,6 +102,7 @@ public:
 	CreateBase(int x, int y, int W, int H) : Fl_Widget(x, y, W, H,0) { }
 
 	void draw() {
+		
 		fl_color(255, 255, 255);
 		int w1 = w();
 		int h1 = h();
@@ -74,15 +112,27 @@ public:
 	}
 };
 
-class CreateCircle : public Fl_Widget {
+class CreateCircles : public Fl_Widget {
 public:
-	CreateCircle(int x, int y) : Fl_Widget(x, y, 0, 0, 0) { }
+	CreateCircles() : Fl_Widget(0, 0, 0, 0, 0) { }
 
 	void draw() {
+		//BX+arms->lines[0]->x1
+
 		fl_color(255, 255, 255);
-		int x1 = x()-5;
-		int y1 = y()-5;
-		fl_pie(x1, y1, 10, 10, 0, 360);
+		//int x1 = (int)arms->lines[0]->x1, BY-arms->lines[0]->y1;
+		//int y1 = y()-5;
+		fl_pie(BX+arms->lines[0]->x1-5, BY-arms->lines[0]->y1-5, 10, 10, 0, 360);
+
+		//int x1 = x()-5;
+		//int y1 = y()-5;
+		fl_pie(BX+arms->lines[1]->x1-5, BY-arms->lines[1]->y1-5, 10, 10, 0, 360);
+
+		//int x1 = x()-5;
+		//int y1 = y()-5;
+		fl_pie(BX+arms->lines[2]->x1-5, BY-arms->lines[2]->y1-5, 10, 10, 0, 360);
+
+
 	}
 };
 
@@ -177,9 +227,13 @@ void j3_cl_callback(Fl_Widget*, void* v) {
 
 void paint_callback(Fl_Widget*, void* v) {
 	painting = !painting;
-	if(painting){printf("Begin painting\n");}
-	if(!painting){printf("End painting\n");}
-	//still need a bloody way to move brush with lines, will discuss during meeting
+	if(painting){
+		printf("Begin painting\n");
+
+	}
+	if(!painting){
+		printf("End painting\n");
+	}
 }
 
 int main(int argc, char **argv) {
@@ -189,23 +243,21 @@ int main(int argc, char **argv) {
 	FillBoxBackground fillBoxBackground(31, 31, 888, 486);
 	Fl_Box drawSpace(30, 30, 890, 488, ""); drawSpace.box(FL_DOWN_FRAME);
 	Fl_Box controlPanel(30, 548, 890, 122, ""); controlPanel.box(FL_DOWN_BOX);
-	
+
 	Drawing d(1,1,4,5);
-	
-	CreateBase base(450,460, 50,30);
 
-	CreateCircle joint1Circ(475, 460);
-	//CreateLine link1(475, 460, 475, 325);
-	
-	CreateCircle joint2Circ(475, 330);
-	//CreateLine link2(475, 325, 475, 225);
+	CreateBase base(450,470, 50,30);
 
-	CreateCircle joint3Circ(475, 230);
-	//CreateLine link3(475, 225, 475, 150);
-
-	CreateBrush brush(475, 150);
-	
 	arms = new Lines();
+
+	CreateCircles joint1Circ();
+
+	//CreateCircle joint2Circ(BX+arms->lines[1]->x1, BY-arms->lines[1]->y1);
+
+	//CreateCircle joint3Circ(BX+arms->lines[2]->x1, BY-arms->lines[2]->y1);
+
+
+
 
 	Fl_Box joint1_txt(215, 550, 30, 15, "Joint 1");
 	Fl_Button j1_cc(150, 570, 60, 30, "-");
@@ -213,7 +265,7 @@ int main(int argc, char **argv) {
 
 	j1_cc.callback(j1_cc_callback, NULL);
 	j1_cl.callback(j1_cl_callback, NULL);
-	
+
 	Fl_Box joint2_txt(460, 550, 30, 15, "Joint 2");
 	Fl_Button j2_cc(395, 570, 60, 30, "-");
 	Fl_Button j2_cl(495, 570, 60, 30, "+");
@@ -229,18 +281,18 @@ int main(int argc, char **argv) {
 	j3_cl.callback(j3_cl_callback, NULL);
 
 	Fl_Button paint(445, 610, 60, 30, "Paint");
-	
+
 	paint.callback(paint_callback, NULL);
 
-	
-	
+
+
 	joint1_txt.show();
 	joint2_txt.show();
 	joint3_txt.show();
 
 	j1_cc.show();
 	j1_cl.show();
-	
+
 	j2_cc.show();
 	j2_cl.show();
 
@@ -248,7 +300,7 @@ int main(int argc, char **argv) {
 	j3_cl.show();
 
 	paint.show();
-	
+
 	window.show();
 	return Fl::run();
 }
